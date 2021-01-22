c00d4268147a033daf33f8db3f5a1cc25fa4664e
Eugenio Merino <eugeniomerino79@gmail.com>
2021-01-22 16:17:31 -0600

    Add cross orign

--- src/main/java/com/database/system/wsconsumedatabase/controller/UsuariosController.java
+++ src/main/java/com/database/system/wsconsumedatabase/controller/UsuariosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.database.system.wsconsumedatabase.model.Usuario;
@@ -20,6 +22,7 @@ import com.database.system.wsconsumedatabase.service.UsuariosService;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class UsuariosController {
 
 	@Autowired
